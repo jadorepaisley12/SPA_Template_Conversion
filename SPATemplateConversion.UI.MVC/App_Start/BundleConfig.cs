e815c390fc9a0e867a1b856c4130e7934c39fae3
Jenna Beckett <jadorepaisley12@gmail.com>
2021-03-08 15:19:39 -0600

    Initial Commit

---
+++ SPATemplateConversion.UI.MVC/App_Start/BundleConfig.cs
@@ -0,0 +1,47 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace SPATemplateConversion.UI.MVC
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Content/vendor/jquery/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/templateScripts").Include(
+                    "~/Content/vendor/jquery.easing/jquery.easing.min.js",
+                    "~/Content/vendor/php-email-form/validate.js",
+                    "~/Content/vendor/waypoints/jquery.waypoints.min.js",
+                    "~/Content/vendor/counterup/counterup.min.js",
+                    "~/Content/vendor/isotope-layout/isotope.pkgd.min.js",
+                    "~/Content/vendor/venobox/venobox.min.js",
+                    "~/Content/vendor/owl.carousel/owl.carousel.min.js",
+                    "~/Content/vendor/aos/aos.js", 
+                    "~/Scripts/main.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/Content/vendor/icofont/icofont.min.css",
+                      "~/Content/vendor/boxicons/css/boxicons.min.css",
+                      "~/Content/vendor/venobox/venobox.css",
+                      "~/Content/vendor/owl.carousel/assets/owl.carousel.min.css",
+                      "~/Content/vendor/aos/aos.css",
+                      "~/Content/style.css"));
+        }
+    }
+}
---
+++ SPATemplateConversion.UI.MVC/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using SPATemplateConversion.UI.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SPATemplateConversion.UI.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult ContactAjax(ContactViewModel cvm)
+        {
+            string body = $"You have received an email from <strong>{cvm.Name}</strong>. The email address given was <strong>{cvm.Email}</strong>.<br/><strong>The following message was sent:</strong> {cvm.Message}";
+
+            MailMessage mm = new MailMessage();
+            mm.From = new MailAddress("email@domain.ext");
+            mm.To.Add(new MailAddress("email@email.com"));
+            mm.Subject = cvm.Subject;
+            mm.Body = body;
+
+            mm.IsBodyHtml = true;
+            mm.ReplyToList.Add(cvm.Email);
+
+            SmtpClient smtp = new SmtpClient("mail.domain.ext");
+            NetworkCredential cred = new NetworkCredential("email@domain.ext", "password");
+
+            smtp.Credentials = cred;
+            smtp.Send(mm);
+            return Json(cvm); 
+        }
+
+
+        public PartialViewResult ContactConfirmation(string name, string email)
+        {
+            ViewBag.Name = name;
+            ViewBag.Email = email;
+            return PartialView("ContactConfirmation"); 
+        }
+    }
+}
